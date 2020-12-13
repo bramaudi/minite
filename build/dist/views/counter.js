d499fb58afbc218a24b978a8ae771e83670bc146
Brama Udi <bramaudi.al@gmail.com>
2020-12-14 01:33:44 +0700

    clear build folder

--- build/dist/views/counter.js
+++
@@ -1,7 +0,0 @@
-import * as React from "../../web_modules/preact.js";
-import {useState} from "../../web_modules/preact/hooks.js";
-function Counter() {
-  const [count, setCount] = useState(0);
-  return /* @__PURE__ */ h("div", null, /* @__PURE__ */ h("h1", null, "Counter"), /* @__PURE__ */ h("p", null, "Count: ", count));
-}
-export default Counter;
