a9a527118df71fc026ab079eaed4f23873b735a1
Brama Udi <bramaudi.al@gmail.com>
2021-01-04 19:02:20 +0700

    remove useEffect export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { createElement as m } from './element.js'
-export { useState, useEffect } from './hooks.js'
+export { useState } from './hooks.js'
 export { useReducer } from './reducer.js'
 export { render } from './render.js'
